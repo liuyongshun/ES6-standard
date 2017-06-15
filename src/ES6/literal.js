14af979b5b35b2fb0fb3253fbd45b62c9fd780c3
liuyongshun <liuys0902@gmail.com>
2017-06-15 17:14:12 +0800

    modify

--- src/ES6/literal.js
+++ src/ES6/literal.js
@@ -1,14 +1,16 @@
 // The identifier of search @@@@literal
 
-// 快级作用域变量和普通变量let & var
+// 一、快级作用域：
 
+// 变量和普通变量let & var==========================================
 // 问题1：
-let numChunkScope = 5;
-for (let i = 0; i < 10; i++) {}
+// 块级作用域，导致外部无法访问到i
+// let numChunkScope = 5;
+// for (let i = 0; i < 10; i++) {}
 // console.log(i); // not defined
-var num = 2;
-for (var i = 0; i < 10; i++) {}
-console.log(i); // 10
+// var num = 2;
+// for (var i = 0; i < 10; i++) {}
+// console.log(i); // 10
 
 // 问题2：
 // let arrByES6 = [];
@@ -44,20 +46,98 @@ console.log(i); // 10
 // bar();
 
 // 问题4：
-// 不允许重复声明变量,而且let声明的不属于window
-// function () {
+// let const 不允许重复声明变量,而且let声明的不属于window
+// function ss () {
 //   let a = 10;
 //   var a = 1;
 // }
 
 // 常量（不可更改）
-const NUM_STABLE = 1;
+// const NUM_STABLE = 1;
 
-/** first:
- * The export method of multiple variable export ，and import is the same as the export
- */
-export {
-  numChunkScope,
-  num,
-  NUM_STABLE
-};
+// 二、变量解构：
+
+// 数组解构赋值,等号的右边不是数组(或者严格地说，不是可遍历的结构)将会报错。================================
+// 1、数组解构赋值
+// var [a, b, , c] = [1, 2, 3, 6];
+// let [d, [[e], f]] = [1, [[2], 3]];
+// let [g, ...h] = [1, 2, 3, 4];
+// let [i, [j], k] = [1, [2, 3], 4];
+// const [x, y, ...z] = ['a'];
+
+// console.log('a:' + a + ';' + typeof a);
+// console.log('b:' + b + ';' + typeof b);
+// console.log('c:' + c + ';' + typeof c);
+// console.log('d:' + d + ';' + typeof d);
+// console.log('e:' + e + ';' + typeof e);
+// console.log('f:' + f + ';' + typeof f);
+// console.log('g:' + g + ';' + typeof g);
+// console.log('h:' + h + ';' + typeof h);
+// console.dir(h);
+// console.log('i:' + i + ';' + typeof i);
+// console.log('j:' + j + ';' + typeof j);
+// console.log('k:' + k + ';' + typeof k);
+// console.log('x:' + x + ';' + typeof x);
+// console.log('y:' + y + ';' + typeof y);
+// console.log('z:' + z + ';' + typeof z);
+// console.dir(z);
+
+// 2、默认值:
+// 左侧赋值则为默认值，右侧再次赋值会覆盖，但是undefined不能覆盖。
+// let [x, y = 'b', z = 33, m = 11] = ['a', undefined, 'cover', null];
+// console.log(x, y, z, m);
+
+// 3、对象解构赋值
+// 注意：数组的元素是按次序排列的，变量的取值由它的位置决定；而对象的属性没有次序，变量必须与属性同名(如果变量不同名字，则匹配模式需要相同，用+++++++++标出的内容)
+// var { bar, foo } = { foo: 'aaa', bar: 'bbb' };
+// console.log(foo);
+// console.log(bar);
+// +++++++++对象的解构赋值是下面形式（键值对）的简写,f和l变成了变量。
+// let obj = { first: 'hello', last: 'world' };
+// let { first: f, last: l } = obj;
+// console.log(f + ' ' + l);
+// console.log(first); // 这里eslint提示错误，未定义first。对象的解构赋值的内部机制，是先找到同名属性，然后再赋给对应的变量。真正被赋值的是后者，而不是前者。
+// 注意：let命令下面一行的圆括号是必须的，否则会报错。因为解析器会将起首的大括号，理解成一个代码块，而不是赋值语句。
+// let get;
+// ({get} = {get: 1});
+// console.log(get);
+
+// 4、字符串，布尔解构
+// const [a, b, c, d, e] = 'hello';
+// console.log(a, b, c, d, e);
+// 解构赋值时，如果等号右边是数值和布尔值，则会先转为对象。
+// let {toString: s} = 123;
+// console.log(s);
+// console.log(s === Number.prototype.toString);
+// 解构赋值的规则是，只要等号右边的值不是对象，就先将其转为对象。由于undefined和null无法转为对象，所以对它们进行解构赋值，都会报错。
+
+// 5、函数参数的解构赋值
+// function add ([x, y]) {
+//   return x + y;
+// }
+// console.log(add([1, 2]));
+
+// 6、圆括号：可以使用情况只有一种：赋值语句的非模式部分，可以使用圆括号。
+
+// 三、变量解构用途
+// 1、变量交换
+let x = 3;
+let y = 5;
+let judge = true;
+if (judge) {
+  [x, y] = [y, x];
+  console.log(x, y);
+}
+
+// 2、函数返回多个值
+function example () {
+  return [1, 2, 3];
+}
+var [a, b, c] = example();
+console.log(a, b, c);
+
+// export {
+//   numChunkScope,
+//   num,
+//   NUM_STABLE
+// };
--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 // first: import method
-import * as pub from './ES6/literal';
+// import * as pub from './ES6/literal';
+import './ES6/literal';
 import './css/index.styl';
 import './css/indexdd.css';
 // console.log(pub.numChunkScope);
 // console.log(pub.NUM_STABLE);
 // console.log(pub.num);
-console.log(pub);
+// console.log(pub);
 
 // second: import method
 // import { numChunkScope, NUM_STABLE, num } from './ES6/literal';
