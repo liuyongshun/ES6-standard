e68bc4b8dbe2481490b76cb5e9995bfd75c95514
liuyongshun <liuys0902@gmail.com>
2017-09-07 12:02:26 +0800

    commit everyday

--- src/ES6/function.autosave.js
+++ src/ES6/function.autosave.js
@@ -14,7 +14,7 @@ setValue();
 function foo ({x, y = 5}) {
   console.log(x, y);
 }
-foo({}); 
+foo({});
 foo({x: 1}) // 1, 5
 foo({x: 1, y: 2}) // 1, 2
 foo() // TypeError: Cannot read property 'x' of undefined
